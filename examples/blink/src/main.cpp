664db1345dc4cd210df9a6e3985366cdab7e2abd
josaezlopez <josaezlopez@gmail.com>
2023-08-10 22:38:59 +0200

    Add example Blink

---
+++ examples/blink/src/main.cpp
@@ -0,0 +1,67 @@
+#include <threadesp32.h>
+
+class Blink : public TaskParent{
+
+  public:
+
+    Blink(const char* _taskName, uint8_t _pin, bool _activeHigh=true): TaskParent(_taskName,1000,TaskParent::th_MEDIUM_PRIORITY,1){
+      pin = _pin;
+      activeHigh = _activeHigh;
+      
+      pinMode(pin,OUTPUT);
+      digitalWrite(pin,!activeHigh);
+    }
+
+
+    void on(float frequency,float workCycle){
+      uint32_t period = 1000 / frequency;
+      tOn = period * workCycle;
+      tOff = period - tOn;
+      resume();
+    }
+
+    void stop(){
+      digitalWrite(pin,!activeHigh);
+      suspend();
+    }
+
+  void loop(){
+    stop();
+    while(true){
+        digitalWrite(pin, activeHigh);
+        delay(tOn);
+        digitalWrite(pin, !activeHigh);
+        delay(tOff);
+      }
+    
+
+  }
+
+
+  private:
+
+    uint32_t tOn = 500;   
+    uint32_t tOff = 500;
+    bool activeHigh;
+    uint8_t pin;
+
+
+
+};
+
+Blink led("TaskLed",LED_BUILTIN);
+
+void setup() {
+  Serial.begin(115200);
+  led.on(2.0,0.5);
+  delay(10000);
+  led.stop();
+ 
+}
+
+
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
+
